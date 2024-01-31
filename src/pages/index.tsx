8f5799827a8fc20e64293c3acd39593ef5739394
Febin-Joseph <febinachu123@gmail.com>
2024-01-31 14:02:43 +0530

    added page routes

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Home from "./Home";
+import Home2 from "./Home2";
+import Host from "./Host";
+import InviteFrnd from "./InviteFrnd";
+import LastPage from "./LastPage";
+import ReferralCode from "./ReferralCode";
+import SocialFeed from "./SocialFeed";
+
+export {
+    Home,
+    Home2,
+    Host,
+    InviteFrnd,
+    LastPage,
+    ReferralCode,
+    SocialFeed,
+}
---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home, Home2, Host, InviteFrnd, LastPage, ReferralCode, SocialFeed } from "../pages";
+
+const AppRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index path="/" element={<Home />}></Route>
+        <Route path="/step2" element={<Home2 />}></Route>
+        <Route path="/social" element={<SocialFeed />}></Route>
+        <Route path="/invite" element={<InviteFrnd />}></Route>
+        <Route path="/code" element={<ReferralCode />}></Route>
+        <Route path="/host" element={<Host />}></Route>
+        <Route path="/last" element={<LastPage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default AppRoutes
