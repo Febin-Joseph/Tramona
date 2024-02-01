f2f32bcc15e640eaf70537c4bb4fa45fa8d37f6e
Febin-Joseph <febinachu123@gmail.com>
2024-02-01 13:47:10 +0530

    completed last page

--- src/components/InviteInput.tsx
+++ src/components/InviteInput.tsx
@@ -5,7 +5,7 @@ const InviteInput: FC = () => {
     <div>
       <h5 className="text-gray-10 font-medium text-[14px] tracking-wider">Your referral code</h5>
       <div className="border-2 border-gray-20 w-[285px] h-[60px] rounded-1xl mt-4">
-        <h1 className="text-[40px] font-medium text-gray-10 tracking-[12px]">FHJENX</h1>
+        <h1 className="text-[40px] font-medium text-gray-10 tracking-[12px] uppercase">FHJENX</h1>
       </div>
     </div>
   )
--- src/pages/LastPage.tsx
+++ src/pages/LastPage.tsx
@@ -1,7 +1,26 @@
-const LastPage = () => {
-  return (
-    <div>LastPage</div>
-  )
-}
+import { FC } from "react";
+import { Button, Timeline } from "../components";
+import { useNavigate } from "react-router-dom";
 
-export default LastPage
+const LastPage: FC = () => {
+  const navigate = useNavigate();
+
+  const handleContinue = () => {
+    navigate('/');
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center pt-10">
+      <Timeline data={5} />
+      <div className="mt-80">
+        <Button
+          label="Back to Start"
+          color="bg-blue-10"
+          onClick={handleContinue}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default LastPage;
